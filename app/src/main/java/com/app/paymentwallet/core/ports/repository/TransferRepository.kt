e695f4be05083014561810b48b64640b91b74dfb
Tadeu Jenuario <44622499+Gnoario@users.noreply.github.com>
2026-01-04 23:50:57 -0300

    refactor: extract attributes to a model

--- app/src/main/java/com/app/paymentwallet/core/ports/repository/TransferRepository.kt
+++ app/src/main/java/com/app/paymentwallet/core/ports/repository/TransferRepository.kt
@@ -1,12 +1,9 @@
 package com.app.paymentwallet.core.ports.repository
 
-import com.app.paymentwallet.core.domain.model.Money
 import com.app.paymentwallet.core.domain.model.Transfer
 
 interface TransferRepository {
-    fun invoke(
-        payerId: String,
-        payeeId: String,
-        amount: Money
+    fun send(
+        transfer: Transfer,
     ): Result<Transfer>
 }
