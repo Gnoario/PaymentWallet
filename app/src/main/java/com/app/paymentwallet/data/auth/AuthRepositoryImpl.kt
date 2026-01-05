73cce6d3a166165781bcdb4de6431ac6974a8937
Tadeu Jenuario <44622499+Gnoario@users.noreply.github.com>
2026-01-04 23:52:10 -0300

    feat: add auth repository implementation

---
+++ app/src/main/java/com/app/paymentwallet/data/auth/AuthRepositoryImpl.kt
@@ -0,0 +1,52 @@
+package com.app.paymentwallet.data.auth
+
+import com.app.paymentwallet.core.data.ApiResult
+import com.app.paymentwallet.core.domain.model.AuthToken
+import com.app.paymentwallet.core.domain.model.PersonInfo
+import com.app.paymentwallet.core.domain.model.User
+import com.app.paymentwallet.core.domain.model.UserId
+import com.app.paymentwallet.core.network.ApiClient
+import com.app.paymentwallet.core.network.ApiRoutes
+import com.app.paymentwallet.core.ports.repository.AuthRepository
+import com.app.paymentwallet.framework.di.Container
+import org.json.JSONObject
+import kotlin.getValue
+
+class AuthRepositoryImpl() : AuthRepository {
+
+    private val apiClient: ApiClient by Container.delegate()
+
+    override fun login(email: String, password: String): Result<User> = runCatching {
+
+        val body = JSONObject().put("email", email).put("password", password).toString()
+
+        val response = apiClient.post(ApiRoutes.LOGIN, body)
+
+        when (response.statusCode) {
+            200, 201 -> {
+                val o = JSONObject(response.body!!)
+                val userId = UserId(o.getString("id"))
+                val personInfo = PersonInfo(
+                    id = userId.value, name = o.getString("name"), email = o.getString("email")
+                )
+                val token = AuthToken(
+                    value = o.getString("authToken"),
+                    createdAtEpochMillis = o.getLong("tokenCreatedAt")
+                )
+                User(
+                    id = userId, info = personInfo, authToken = token
+                )
+            }
+
+            else -> throw ApiResult.Failure(response.statusCode).toException()
+        }
+    }
+
+    override fun getSession(): Result<User> {
+        TODO("Not yet implemented")
+    }
+
+    override fun logout(): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+}
